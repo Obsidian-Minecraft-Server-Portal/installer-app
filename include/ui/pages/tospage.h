717c20512808267b2ccfeee7d5914c9c8fc39a99
Drew Chase (Home) <dcmanproductions@gmail.com>
2025-11-09 14:09:01 -0500

    Add Terms of Service (TOS) page to installer UI
    Introduced a new TOS page with a user-friendly layout including a label, text edit for terms content, checkbox for acceptance, and navigation buttons for moving back or continuing. Connected navigation actions to `InstallerWindow` functionality. Updated `.h`, `.cpp`, and `.ui` files accordingly.

---
+++ include/ui/pages/tospage.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#ifndef OBSIDIAN_INSTALLER_TOSPAGE_H
+#define OBSIDIAN_INSTALLER_TOSPAGE_H
+
+#include "ui_TOSPage.h"
+
+namespace ObsidianInstaller {
+    QT_BEGIN_NAMESPACE
+
+    namespace Ui {
+        class TOSPage;
+    }
+
+    QT_END_NAMESPACE
+
+    class TOSPage final : public QWidget {
+        Q_OBJECT
+
+    public:
+        explicit TOSPage(QWidget *parent = nullptr);
+
+        ~TOSPage() override;
+
+    private:
+        Ui::TOSPage *ui;
+        void onBack() const;
+        void onNext() const;
+    };
+} // ObsidianInstaller
+
+#endif //OBSIDIAN_INSTALLER_TOSPAGE_H
---
+++ src/ui/pages/tospage.cpp
@@ -0,0 +1,22 @@
+#include "ui/pages/tospage.h"
+#include <ui/installerwindow.h>
+
+namespace ObsidianInstaller {
+    TOSPage::TOSPage(QWidget *parent) : QWidget(parent), ui(new Ui::TOSPage)  {
+        ui->setupUi(this);
+        connect(ui->backButton, &QPushButton::clicked, this, &TOSPage::onBack);
+        connect(ui->nextButton, &QPushButton::clicked, this, &TOSPage::onNext);
+    }
+
+    TOSPage::~TOSPage() {
+        delete ui;
+    }
+
+    void TOSPage::onBack() const {
+        reinterpret_cast<InstallerWindow *>(window())->decrementPage();
+    }
+
+    void TOSPage::onNext() const {
+        reinterpret_cast<InstallerWindow *>(window())->incrementPage();
+    }
+} // ObsidianInstaller
